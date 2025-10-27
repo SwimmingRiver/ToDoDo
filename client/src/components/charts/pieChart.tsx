c62a941f8a293e4644a2ebec2a58d67fec7a74cb
swimmingRiver <swimmingr@gmail.com>
2025-10-27 20:59:01 +0900

    feat: 리사이징 버그 수정

--- client/src/components/charts/pieChart.tsx
+++ client/src/components/charts/pieChart.tsx
@@ -1,9 +1,12 @@
 import { styled } from "styled-components";
-import { Cell, Pie, PieChart, Tooltip } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
 const PieChartContainer = styled.div`
   width: 100%;
   height: 100%;
+  min-width: 0px;
+  min-height: 0px;
+  overflow: hidden;
 `;
 const data = [
   { name: "Todo", value: 400 },
@@ -14,23 +17,27 @@ const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 const PieChartComponent = () => {
   return (
     <PieChartContainer>
-      <PieChart width="100%" height="100%" responsive>
-        <Pie
-          label={({ name, value }) => `${name}: ${value}`}
-          data={data}
-          cx="50%"
-          cy="50%"
-          innerRadius={0}
-          outerRadius={100}
-          dataKey="value"
-          nameKey="name"
-        >
-          {data.map((entry, index) => (
-            <Cell key={entry.name} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        <Tooltip contentStyle={{ backgroundColor: "white", color: "black" }} />
-      </PieChart>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie
+            label={({ name, value }) => `${name}: ${value}`}
+            data={data}
+            cx="50%"
+            cy="50%"
+            innerRadius={0}
+            outerRadius="70%"
+            dataKey="value"
+            nameKey="name"
+          >
+            {data.map((entry, index) => (
+              <Cell key={entry.name} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip
+            contentStyle={{ backgroundColor: "white", color: "black" }}
+          />
+        </PieChart>
+      </ResponsiveContainer>
     </PieChartContainer>
   );
 };
