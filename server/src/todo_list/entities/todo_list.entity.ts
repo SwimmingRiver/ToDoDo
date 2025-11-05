daa84bb786abd97fc6749249b2768d56d5d26796
swimmingRiver <swimmingr@gmail.com>
2025-11-05 23:46:43 +0900

    feat: 클라이언트와 타입 통일을 위한 엔티티 변경

--- server/src/todo_list/entities/todo_list.entity.ts
+++ server/src/todo_list/entities/todo_list.entity.ts
@@ -9,8 +9,26 @@ export class TodoList extends Document {
   @Prop({ required: false })
   description: string;
 
-  @Prop({ required: true, default: 'pending' })
+  @Prop({ required: true, default: 'todo', enum: ['todo', 'doing', 'done'] })
   status: string;
+
+  @Prop({ required: false, default: null })
+  startAt: Date;
+
+  @Prop({ required: false, default: null })
+  dueAt: Date;
+
+  @Prop({ required: false, default: null })
+  doneAt: Date;
+
+  @Prop({ required: true, default: 'medium', enum: ['low', 'medium', 'high'] })
+  priority: string;
+
+  @Prop({ required: false, default: null })
+  parentId: string;
+
+  @Prop({ required: true, default: 0 })
+  order: number;
 }
 
 export const TodoListSchema = SchemaFactory.createForClass(TodoList);
