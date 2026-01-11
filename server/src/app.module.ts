799054c79c74ff28cb59149f7e85ef47ba73437e
swimmingRiver <swimmingr@gmail.com>
2026-01-11 18:16:52 +0900

    feat: 환경 변수 적용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoListModule } from './todo_list/todo_list.module';
@@ -6,8 +7,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TodoListModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/todo-list'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGODB_URI') ??
+          'mongodb://localhost:27017/todo-list',
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
