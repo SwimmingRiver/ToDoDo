8a7ec7d46335704fe260a5f0bfc08f297218ff97
swimmingRiver <swimmingr@gmail.com>
2026-01-13 00:30:29 +0900

    feat: 이미지 적용

--- client/src/layouts/header/header.tsx
+++ client/src/layouts/header/header.tsx
@@ -1,11 +1,17 @@
 import { styled } from "styled-components";
+import logo from "../../assets/logo.png";
+
 const HeaderContainer = styled.header`
   width: 100%;
   border-bottom: 1px solid #e0e0e0;
   padding: 10px;
 `;
 const Header = () => {
-  return <HeaderContainer>header</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="logo" height={48} />
+    </HeaderContainer>
+  );
 };
 
 export default Header;
